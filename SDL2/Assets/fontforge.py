cfa56d39587482c2382320abbb7737c5e10ac982
AndriusDap <Dapsevicius.Andrius@gmail.com>
2014-05-10 12:21:36 +0300

    fontforge

--- SDL2/Assets/fontforge.py
+++ SDL2/Assets/fontforge.py
@@ -5,25 +5,13 @@ from PIL import Image
 from PIL import ImageDraw
 
 
-letters = ' 1234567890,.;:"\'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM'
-description = open('font_description.txt', 'w')
-font = ImageFont.truetype("DroidSans.ttf", 32)
-image = Image.new("RGBA", (2048, 64), (0, 0, 0, 0))
-canvas = ImageDraw.Draw(image)
-cursor = 0
-total_width = 2048
-description.write('[height]: {}\n'.format(32/total_width))
+letters = '1234567890,.qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM'
+font = ImageFont.truetype("DroidSans.ttf", 52)
 
-def describe(letter, start, end):
-	start = start / total_width
-	end = end / total_width
-	description.write('[{}]: {} {}\n'.format(letter, start, end))
 
 for letter in letters:
+	image = Image.new("RGBA", (64, 64), (0, 0, 0, 0))
+	canvas = ImageDraw.Draw(image)
 	(width, height) = canvas.textsize(letter, font=font)
-	describe(letter, width, cursor + width)
-	canvas.text((cursor, 0), letter, (255, 255, 255, 255), font=font)
-	cursor = cursor + width
-
-print(cursor)
-image.save("DroidSans.png")
+	canvas.text((0, 0), letter, (20, 255, 20, 255), font=font)
+	image.save("SpriteSheetContent/" + letter + ".png", format="png")
